d53ece3a00f95a191e6ee041d6243b4f7b6e07b0
OA Wu <comdan66@gmail.com>
2017-04-04 22:05:07 +0800

    Edit readme file.

--- application/views/content/site/main/index/content.php
+++ application/views/content/site/main/index/content.php
@@ -39,7 +39,7 @@
         <h2><?php echo Lang::get ('index', 'mixed');?></h2>
         <div class='items'>
     <?php foreach ($products as $product) { ?>
-            <a href="">
+            <a href="<?php echo base_url ('product', $product->id);?>">
               <img src='<?php echo $product->cover1->url ('500x500c');?>'>
               <span><?php echo $product->title;?></span>
             </a>
